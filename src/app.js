917d6dda93bc236923b6bda1a7948e076432e15c
Savo Vuksan <savovuksan@gmail.com>
2019-09-24 22:46:03 +0200

    Implemented login/logout Lifecycle

--- src/app.js
+++ src/app.js
@@ -1,6 +1,51 @@
 import {User} from './User.js';
 import { Ride } from './Ride.js';
-const user = new User('savo', '12345');
-const ride = new Ride('Linz','Marchtrenk', user, 1234,1245);
-console.log(user);
-console.log(ride);
+
+// Firebase objects
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+// DOM Vars
+const loginForm = document.querySelector('.loginForm');
+const loginContainer = document.querySelector('.login-container');
+const appContainer = document.querySelector('.app-container');
+const signOutBtn = document.querySelector('.signOut-btn');
+// Vars
+let isLoggedIn = false;
+
+// Functions
+const login = (email, password) => {
+    auth.signInWithEmailAndPassword(email, password)
+    .then(userCred => console.log(userCred))
+    .catch(err => console.log(err));
+};
+
+const toggleLoginForm = (isLoggedIn) => {
+    if(isLoggedIn){
+        loginContainer.classList.add('d-none');
+        loginForm.reset();
+        appContainer.classList.toggle('d-none');
+    }else{
+        loginContainer.classList.remove('d-none');
+        loginForm['login-email'].focus();
+    }
+}
+
+//  Event Listeners
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    login(loginForm['login-email'].value, loginForm['login-password'].value);
+});
+
+signOutBtn.addEventListener('click', e => {
+    auth.signOut();
+    appContainer.classList.add('d-none');
+});
+
+
+// Auth state listener
+auth.onAuthStateChanged(user => {
+    isLoggedIn = (user !== null ? true : false);
+    
+    toggleLoginForm(isLoggedIn);
+});
